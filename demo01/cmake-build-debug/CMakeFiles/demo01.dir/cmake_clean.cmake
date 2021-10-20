5b78813a6d50ec9b99e2f35774201cc4c6457bed
hatrongvu13 <hatrongvu13@gmail.com>
2021-10-20 17:00:46 +0700

    code demo

---
+++ demo01/cmake-build-debug/CMakeFiles/demo01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo01.dir/main.c.o"
+  "demo01"
+  "demo01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ demo01/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("jax tony !");
+    int arr[100][100];
+    arr[0][0] = 1;
+    printf("%d", arr[0][0]);
+    return 0;
+}
